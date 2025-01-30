df3e76901e16c7eea9a5df5151a5f7bb401e867d
dmkannan30 <duraikannan1830@gmail.com>
2025-01-30 22:00:17 +0530

    changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom"
 const Header = () => {
     return (
         <header className="Header">
-            <h1>Mugu Blogsss</h1>
+            <h1>Mugu Blog</h1>
             <nav>
                 <ul>
                     <li><Link to="/">Home</Link></li>
